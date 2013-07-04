cbad80f6e3d90b8f0d4df349628331c0dba8bc0c
jean-home <mahai_1986@126.com>
2013-07-04 21:12:08 +0800

    add ciphertext service and unit test.

---
+++ utility.net/utility.net/Net.Utility/Security/CiphertextService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Net.Utility.Security
+{
+    public class CiphertextService
+    {
+        public static string MD5Encryption(string input)
+        {
+            var md5 = MD5.Create();
+            var bytes = Encoding.UTF8.GetBytes(input);
+            var hashValue= md5.ComputeHash(bytes);
+            var result = new StringBuilder();
+            foreach (var item in hashValue)
+            {
+                result.Append(item.ToString("x2"));
+            }
+            return result.ToString();
+        }
+        public static bool ValidateMD5(string input,string hashValue)
+        {
+            return MD5Encryption(input)==hashValue;
+        }
+    }
+}
---
+++ utility.net/utility.net/Utility.Test/UtilityTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Net.Utility.Security;
+
+namespace Utility.Test
+{
+    [TestClass]
+    public class UtilityTest
+    {
+        [TestMethod]
+        public void MD5Encryption()
+        {
+            Console.WriteLine(CiphertextService.MD5Encryption("test"));
+        }
+        [TestMethod]
+        public void ValidateMD5()
+        {
+            Console.WriteLine(CiphertextService.ValidateMD5("test","098f6bcd4621d373cade4e832627b4f6"));
+        }
+    }
+}
