9a97b6d79c004a71d9ca09a7490fe319e9680dce
jeanmahai <mahai_1986@126.com>
2013-07-05 14:33:35 +0800

    create nhibernate.utility

---
+++ utility.net/utility.net/Utility.Test/NHibernate.Utility.Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHibernate.Utility;
+using Net.Utility.Security;
+
+namespace Utility.Test
+{
+    [TestClass]
+    public class NHibernateUtilityTest
+    {
+        [TestMethod]
+        public void ShowSqlsTest()
+        {
+            foreach (var item in SqlManager.GetAllText())
+            {
+                Console.WriteLine(string.Format("name:{0},\n sql:\n{1}",item.Key,item.Value));
+            }
+            //var a = SqlManager.GetSqlText("a");
+            //Console.WriteLine(CiphertextService.MD5Encryption("test"));
+        }
+        [TestMethod]
+        public void GetSqlTextTest()
+        {
+            Console.WriteLine(SqlManager.GetSqlText("test"));
+        }
+    }
+}
