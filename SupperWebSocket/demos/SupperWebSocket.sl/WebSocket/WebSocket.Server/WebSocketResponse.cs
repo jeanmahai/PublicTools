36756bbc49060b0632d56bc6358dc222468a8b53
jean-home <mahai_1986@126.com>
2013-07-17 23:53:59 +0800

    1

--- SupperWebSocket/demos/SupperWebSocket.sl/WebSocket/WebSocket.Server/WebSocketResponse.cs
+++ SupperWebSocket/demos/SupperWebSocket.sl/WebSocket/WebSocket.Server/WebSocketResponse.cs
@@ -5,7 +5,14 @@ namespace WebSocket.WebSocket.Server
     [Serializable]
     public class WebSocketResponse
     {
+        /// <summary>
+        /// 客户端执行的脚本id
+        /// </summary>
         public string Handler { get; set; }
         public string Data { get; set; }
+        /// <summary>
+        /// 服务端直接返回脚本执行
+        /// </summary>
+        public string RemoteHandler { get; set; }
     }
 }
