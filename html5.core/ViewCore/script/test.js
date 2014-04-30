949a16afb93af44443bd49e80266eca6a96b23c1
jeanmahai <mahai_1986@126.com>
2014-04-30 13:07:49 +0800

    sdf

--- html5.core/ViewCore/script/test.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by jm96 on 14-4-24.
- */
-function test(){}
--- html5.core/boolean.parse.js
+++
@@ -1,5 +0,0 @@
-if (!Boolean.parse) {
-	Boolean.parse = function (val) {
-		return new RegExp("^true$", "i").test(val);
-	};
-}
